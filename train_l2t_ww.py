4212d37840a4357d09a13a3c852a5703af282761
aluex <aluex@outlook.com>
2020-04-18 18:52:10 -0400

    minor

--- train_l2t_ww.py
+++ train_l2t_ww.py
@@ -126,8 +126,9 @@ class LossWeightNetwork(nn.ModuleList):
 def main(mimicLoader=None, arguments=None):
 
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('--num-classes', default=4)
+    
     parser.add_argument('--dataroot', required=True, help='Path to the dataset')
+    parser.add_argument('--num-classes', type=int, default=4)
     parser.add_argument('--dataset', default='cub200')
     parser.add_argument('--datasplit', default='cub200')
     parser.add_argument('--batchSize', type=int, default=64, help='Input batch size')
